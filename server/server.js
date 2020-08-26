31851ec602b42141f5e186efa2973069aba265be
scott-olson <scotteolson3@gmail.com>
2020-08-26 14:35:21 -0700

    add routes

--- server/server.js
+++ server/server.js
@@ -8,31 +8,58 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 // slowdown middleware
 const slowDown = require('express-slow-down');
-
+// path middleware
 const path = require('path');
-
 
 const app = express();
 
+app.use((req, res, next) => {
+	console.log("hit the middleware");
+	next();
+});
 
 const port = process.env.PORT || 4001;
 
-app.listen(port, () => {
-	console.log(`The server is up and running on port ${port}`);
-});
 
 // get all recipes
-app.get("/", (req, res) => {
+app.get("/api/recipes", (req, res) => {
+	console.log("Get all recipes");
 	res.json({
-		status: "success",
-		recipes: []
-	})
+		status: "success, get landing",
+		data: {
+			recipes: [],
+			code: []
+		}
+	});
 });
 
-app.get("/", (req, res) => {
-	console.log("Get all recipes");
+// get a recipe by an id
+app.get("/api/recipes/:id", (req, res) => {
+	console.log("Get recipe by id");
+	console.log(req.params);
+	res.json({
+		status: "success, get by id",
+		data: {
+			recipes: [],
+			code: []
+		}
+	});
 });
 
-app.get("/", (req, res) => {
-	console.log("Get all recipes");
+// create a recipe
+app.post("/api/recipes", (req, res) => {
+	console.log(req.query);
+	res.json({
+		status: "success, create",
+		data: {
+			query: req.query,
+			code: []
+		}
+	});
+});
+
+
+
+app.listen(port, () => {
+	console.log(`The server is up and running on port ${port}`);
 });
