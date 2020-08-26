a2194194dcb9e2af1b5ad007a8836cbc39e387ab
scott-olson <scotteolson3@gmail.com>
2020-08-26 11:53:28 -0700

    add dependencies and initial routes

---
+++ server/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+// logging middleware
+const morgan = require('morgan');
+// security middleware
+const helmet = require('helmet');
+// rate limitting middleware
+const rateLimit = require('express-rate-limit');
+// slowdown middleware
+const slowDown = require('express-slow-down');
+
+const path = require('path');
+
+
+const app = express();
+
+
+const port = process.env.PORT || 4001;
+
+app.listen(port, () => {
+	console.log(`The server is up and running on port ${port}`);
+});
+
+// get all recipes
+app.get("/", (req, res) => {
+	console.log("Get all recipes");
+});
+
+app.get("/", (req, res) => {
+	console.log("Get all recipes");
+});
+
+app.get("/", (req, res) => {
+	console.log("Get all recipes");
+});
